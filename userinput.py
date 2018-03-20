ddb7bc8c5d2344db3fc6bb835b926c50c3a89293
olofs <svedvall@gmail.com>
2018-03-20 10:12:02 +0100

    Gjort om inläsningen av User Input.

--- userinput.py
+++ userinput.py
@@ -1,13 +1,16 @@
 
 
 class UserInput:
+    def __init__(self):
+        self.keywords = ['short castle', 'long castle', 'en passant', 'q', 'quit']
+
     def getMsg(self):
         while True:
             msg = input(">>> ")
+            if msg in self.keywords:
+                return [msg]
             msg = msg.split()
-            if len(msg) == 1:
-                return msg
-            elif len(msg) == 2:
+            if len(msg) == 2:
                 return msg
             else:
-                print('Måste ge 1 eller 2 argument.')
+                print('Måste ge commando - short/long castle, en passant - eller 2 argument.')
